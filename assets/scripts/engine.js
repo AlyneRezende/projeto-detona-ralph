97c7e0a6a005165bd3fee8ceb249f67c4d234d80
Alyne <a.alynerezende@gmail.com>
2024-10-01 17:32:52 +0100

    adicionando contador de vidas

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -4,15 +4,30 @@ const state = {
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"),
+        live: document.querySelector("#lives"),
     },
     values: {
         hitPosition: 0,
         result: 0,
-        currentTime: 30,
+        currentTime: 10,
+        currentLive: 3,
     },
     actions: {
-        countDownTimerId: setInterval(countDown, 1000),
-        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: null, //setInterval(countDown, 1000),
+        timerId: null, //setInterval(randomSquare, 1000),
+        timeOutId: null,
+    }
+}
+
+function loseLive() {
+    state.values.currentLive--;
+    state.view.live.textContent = state.values.currentLive;
+
+    if (state.values.currentLive <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Suas vidas acabaram! Você perdeu!");
+        resetGame();
     }
 }
 
@@ -24,6 +39,11 @@ function countDown() {
         clearInterval(state.actions.countDownTimerId);
         clearInterval(state.actions.timerId);
         alert("Tempo esgotado! O seu resultado foi: " + state.values.result);
+        resetGame();
+    } else {
+        if (state.values.hitPosition !== null) {
+            loseLive();
+        }
     }
 }
 
@@ -38,7 +58,7 @@ function randomSquare() {
         square.classList.remove("enemy");
     });
 
-    let randomNumber = Math.floor(Math.random() * 9);
+    let randomNumber = Math.floor(Math.random() * state.view.squares.length);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
     state.values.hitPosition = randomSquare.id;
@@ -52,13 +72,30 @@ function addListenerHitBox() {
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
                 playSound("hit");
+                clearTimeout(state.actions.timeOutId);
             }
-        })
+        });
     });
+}
+
+function resetGame() {
+    state.values.currentLive = 3;
+    state.values.currentTime = 10;
+    state.values.result = 0;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    state.view.score.textContent = state.values.result;
+    state.view.live.textContent = state.values.currentLive;
+
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+    state.actions.countDownTimerId = setInterval(countDown, 1000);
+    state.actions.timerId = setInterval(randomSquare, 1000);
 }
 
 function initialize() {
     addListenerHitBox();
+    state.actions.countDownTimerId = setInterval(countDown, 1000);
+    state.actions.timerId = setInterval(randomSquare, 1000);
 }
 
 initialize();
